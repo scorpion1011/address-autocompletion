532c21dadba631876a7ef9ecbab5361636f104e3
scorpion1011 <vladislav.nesterenko@gmail.com>
2018-08-10 20:00:58 +0300

    alerts

--- adress_correction.js
+++ adress_correction.js
@@ -1,1 +1,16 @@
-alert ("111");
+jQuery(function() {
+	//jQuery('#billing_company').change(function() {
+	//	alert("111");
+	//});
+	jQuery('#billing_city').on('input', function() {
+		alert("111");
+	});
+	
+	//jQuery('#billing_country').on('change', function() {
+	//	alert("222");
+	//});
+	
+	jQuery('#billing_postcode').on('input', function() {
+		alert("333");
+	});
+});
--- index.php
+++ index.php
@@ -8,6 +8,7 @@
     // Register the script like this for a plugin:
     wp_register_script( 'custom-script', plugins_url() . '/adress_autocompletion/adress_correction.js' );
 	
+	wp_enqueue_script('jquery');
     // For either a plugin or a theme, you can then enqueue the script:
     wp_enqueue_script( 'custom-script' );
 }
