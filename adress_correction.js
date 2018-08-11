5c8af41309e6617d68c2bf44fc2efc6caa19eea7
scorpion1011 <vladislav.nesterenko@gmail.com>
2018-08-11 11:10:00 +0300

    ajax(not completed)

--- adress_correction.js
+++ adress_correction.js
@@ -1,9 +1,14 @@
 jQuery(function() {
-	//jQuery('#billing_company').change(function() {
-	//	alert("111");
-	//});
 	jQuery('#billing_city').on('input', function() {
-		alert("111");
+		var data = {
+		action: 'action',
+		city: jQuery(this).val(),
+		sender: 'city'
+		};
+
+		jQuery.get( myPlugin.ajaxurl, data, function(response) {
+			alert(response);
+		});
 	});
 	
 	//jQuery('#billing_country').on('change', function() {
@@ -11,6 +16,14 @@ jQuery(function() {
 	//});
 	
 	jQuery('#billing_postcode').on('input', function() {
-		alert("333");
+		var data = {
+		action: 'action',
+		zip: jQuery(this).val(),
+		sender: 'zip'
+		};
+
+		jQuery.get( myPlugin.ajaxurl, data, function(response) {
+			alert(response);
+		});
 	});
 });
--- index.php
+++ index.php
@@ -3,15 +3,38 @@
  * Plugin Name: adress autocompletion
  */
  
- function link_script()
+add_action( 'wp_enqueue_scripts', 'link_script' );
+function link_script()
 {
     // Register the script like this for a plugin:
-    wp_register_script( 'custom-script', plugins_url() . '/adress_autocompletion/adress_correction.js' );
+    ///wp_register_script( 'custom-script', plugins_url() . '/adress_autocompletion/adress_correction.js' );
 	
-	wp_enqueue_script('jquery');
+	///wp_enqueue_script('jquery');
     // For either a plugin or a theme, you can then enqueue the script:
-    wp_enqueue_script( 'custom-script' );
+    ///wp_enqueue_script( 'custom-script' );
+	
+	wp_enqueue_script( 'custom-script', plugins_url() . '/adress_autocompletion/adress_correction.js', array('jquery') );
+	
+	wp_localize_script( 'custom-script', 'myPlugin', 
+		array(
+			'ajaxurl' => admin_url('admin-ajax.php')
+		)
+	);  
 }
 
-add_action( 'wp_enqueue_scripts', 'link_script' );
+add_action('wp_ajax_action', 'my_action_callback');
+add_action('wp_ajax_nopriv_action', 'my_action_callback');
+
+function my_action_callback() {
+	$zip = empty( $_GET['zip'] ) ? '' : esc_attr( $_GET['zip'] );
+	$city =  empty( $_GET['city'] ) ? '' : esc_attr( $_GET['city'] );
+	$firstname = empty( $_GET['firstname'] ) ? '' : esc_attr( $_GET['firstname'] );
+	$sender =  empty( $_GET['sender'] ) ? '' : esc_attr( $_GET['sender'] );
+	
+	$result = '[{"zip":"' . $zip . '";"city":"' . $city . '";"firstname":"' . $firstname . '";"sender":"' . $sender . '"}]';
+	 
+	echo $result;
+
+	wp_die();
+}
 ?>
