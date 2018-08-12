60d5fadf48e09e6241a2b615e3bca4ad2768071d
scorpion1011 <vladislav.nesterenko@gmail.com>
2018-08-12 16:33:55 +0300

    autocomplete v2

--- js/adress_correction.js
+++ js/adress_correction.js
@@ -17,7 +17,7 @@ jQuery(function() {
 				jQuery.each( obj, function( key, value ) {
 					if (key = 'city')
 					{
-						arrayOfObjectProperty[arrayOfObjectProperty.length] = value.city;
+						arrayOfObjectProperty[arrayOfObjectProperty.length] = value.city + ', ' + value.postcode;
 					}
 				});
 			}
@@ -35,7 +35,7 @@ jQuery(function() {
 				jQuery.each( obj, function( key, value ) {
 					if (key = 'postCode')
 					{
-						arrayOfObjectProperty[arrayOfObjectProperty.length] = value.postcode;
+						arrayOfObjectProperty[arrayOfObjectProperty.length] = value.postcode + ', ' + value.city;
 					}
 				});
 			}
@@ -49,7 +49,21 @@ jQuery(function() {
 							matches.push(arrayOfObjectProperty[i])
 						};
 					}
+					
 					suggests(matches);
+				},
+				onSelect: function(e, term, item){
+					if (sender == 'city') {
+						//lol, kek, 555
+						var links = term.split(', ');
+						jQuery('#billing_city').val(links[0].trim());
+						jQuery('#billing_postcode').val(links[1].trim());
+					}
+					else if (sender == 'zip') {
+						var links = term.split(',');
+						jQuery('#billing_city').val(links[1].trim());
+						jQuery('#billing_postcode').val(links[0].trim());
+					}
 				}
 			});
 		});
@@ -62,7 +76,43 @@ jQuery(function() {
 		dataRequest('address');
 	});
 	
-	jQuery('#billing_postcode').on('input', function() {
-		dataRequest('zip');
+	//jQuery('#billing_postcode').on('input', function() {
+	//	dataRequest('zip');
+	//});
+	jQuery('#billing_postcode').autoComplete({
+		minChars: 0,
+		source: function(input, suggests) {
+			var data = {
+				action: 'action',
+				address: jQuery('#billing_address_1').val(),
+				city: jQuery('#billing_city').val(),
+				zip: jQuery('#billing_postcode').val(),
+				sender: 'zip'
+			};
+			jQuery.get( myPlugin.ajaxurl, data, function(response) {
+				var arrayOfObjectProperty = [];
+				var obj = jQuery.parseJSON(response);
+				var htmlElement;
+		
+				jQuery.each( obj, function( key, value ) {
+					if (key = 'postCode') {
+						arrayOfObjectProperty[arrayOfObjectProperty.length] = value;//.postcode + ', ' + value.city;
+					}
+				});
+				suggests(arrayOfObjectProperty);
+			});
+		},
+		renderItem: function (item, search){
+			var str = item.postcode + ', ' + item.city;
+			
+			search = search.replace(/[-\/\\^$*+?.()|[\]{}]/g, '\\$&');
+			var re = new RegExp("(" + search.split(' ').join('|') + ")", "gi");
+			return '<div class="autocomplete-suggestion" data-city="' + item.city + '" data-postcode="' + item.postcode + '" data-val="' + str + '">' + str.replace(re, "<b>$1</b>") + '</div>';
+
+		},
+		onSelect: function(e, term, item){
+			jQuery('#billing_city').val(jQuery(item).attr("data-city"));
+			jQuery('#billing_postcode').val(jQuery(item).attr("data-postcode"));
+		}
 	});
 });
