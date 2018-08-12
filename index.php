03490031dd78604123c6ef9c88c8c2d6faf26818
ynesterenko <yurii.nesterenko@gmail.com>
2018-08-12 12:39:48 +0300

    Get Endereco data on ZIP change

--- index.php
+++ index.php
@@ -30,54 +30,52 @@ function my_action_callback()
 {
 	require_once __DIR__.'/sdk/autoload.php';
 
+	// 	$firstname = empty( $_GET['firstname'] ) ? '' : esc_attr( $_GET['firstname'] );
+
 	$sender =  empty( $_GET['sender'] ) ? '' : esc_attr( $_GET['sender'] );
 
 	switch($sender)
 	{
-		case 'zip':
 		case 'city':
 			$data = json_decode('[{"city": "Daaden","postCode": "57567"},{"city": "Daasdorf a Berge","postCode": "99428"},{"city": "Daasdorf b Buttelstedt","postCode": "99439"},{"city": "Dabel","postCode": "19406"},{"city": "Dabergotz","postCode": "16818"},{"city": "Daberkow","postCode": "17129"},{"city": "Dachau","postCode": "85221"},{"city": "Dachrieden","postCode": "99974"},{"city": "Dachsbach","postCode": "91462"},{"city": "Dachsberg","postCode": "79875"}]');
+			break;
+		case 'zip':
+// 			$data = json_decode('[{"city": "Daaden","postCode": "57567"},{"city": "Daasdorf a Berge","postCode": "99428"},{"city": "Daasdorf b Buttelstedt","postCode": "99439"},{"city": "Dabel","postCode": "19406"},{"city": "Dabergotz","postCode": "16818"},{"city": "Daberkow","postCode": "17129"},{"city": "Dachau","postCode": "85221"},{"city": "Dachrieden","postCode": "99974"},{"city": "Dachsbach","postCode": "91462"},{"city": "Dachsberg","postCode": "79875"}]');
+			$zip = empty( $_GET['zip'] ) ? '' : esc_attr( $_GET['zip'] );
+// 			$city =  empty( $_GET['city'] ) ? '' : esc_attr( $_GET['city'] );
+
+			$client = EnderecoClient::getInstance('mobilemojo', 'developer01', 'zG-BE$_9');
+			$postCodeExpansionRequest = new EnderecoPostCodeExpansionRequest();
+
+
+			// 	$postCodeExpansionRequest->setPostcode('972');
+			// 		->setCity('Randersacker')
+			// 		->setHousenumber('4b')
+			// 		->setStreet('Balthasar-Neumann-Straße');
+
+
+			if(!empty($zip))
+			{
+				$postCodeExpansionRequest->setPostcode($zip);
+			}
+// 			if(!empty($city))
+// 			{
+// 				$postCodeExpansionRequest->setCity($city);
+// 			}
+			$result = $client->executeRequest($postCodeExpansionRequest);
+			$data = [];
+			foreach($result->getElements() as $correction)
+			{
+				$data[] = [
+						'postcode' => $correction->getPostCode(),
+						'city'     => $correction->getCity(),
+					];
+			}
 			break;
 		case 'address':
 			$data = json_decode('[{"street": "Eisenlohrsweg"},{"street": "Eppendorfer Baum"},{"street": "Eppendorfer Landstr."}]');
 			break;
 	}
-// 	$zip = empty( $_GET['zip'] ) ? '' : esc_attr( $_GET['zip'] );
-// 	$city =  empty( $_GET['city'] ) ? '' : esc_attr( $_GET['city'] );
-// 	$firstname = empty( $_GET['firstname'] ) ? '' : esc_attr( $_GET['firstname'] );
-
-
-// 	$client = EnderecoClient::getInstance('mobilemojo', 'developer01', 'zG-BE$_9');
-// 	$addressCheckRequest = new EnderecoAddressCheckRequest();
-
-
-// 	$addressCheckRequest->setPostcode('972');
-// 		->setCity('Randersacker')
-// 		->setHousenumber('4b')
-// 		->setStreet('Balthasar-Neumann-Straße');
-
-
-// 	if(!empty($zip))
-// 	{
-// 		$addressCheckRequest->setPostcode($zip);
-// 	}
-// 	if(!empty($city))
-// 	{
-// 		$addressCheckRequest->setCity($city);
-// 	}
-// 	$result = $client->executeRequest($addressCheckRequest);
-
-// 	$data = [];
-// 	foreach($result->getElements() as $correction)
-// 	{
-// 		$data[] = [
-// 				'Postcode'      => $correction->getPostCode(),
-// 				'City'          => $correction->getCity(),
-// 				'Street'        => $correction->getStreet(),
-// 				'House Nr.'     => $correction->getHouseNumber(),
-// 				'Federal State' => $correction->getFederalState(),
-// 			];
-// 	}
 	echo json_encode($data);
 
 // 	$result = '[{"zip":"' . $zip . '";"city":"' . $city . '";"firstname":"' . $firstname . '";"sender":"' . $sender . '"}]';
